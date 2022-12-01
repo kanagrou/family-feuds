76746eb72ea71c4e77999e20ba0dc24e1e99d2bb
Kana <68487042+kanagrou@users.noreply.github.com>
2022-12-01 15:33:51 +0000

    Control panel files

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
+// import * as express from "express";
 const express = require("express");
+const path = require("path");
 
 // Routes
 const play = require("./routes/play.js");
+const admin_route = require("./routes/admin.js");
 
 const PORT = 3000;
 const app = express();
 const server = app.listen(PORT, () => console.log("Listening on port", PORT));
 
 // Settings
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Routes middleware
 app.use("/play", play);
+app.use("/admin", admin_route);
 
 app.get("/", (req, res) => {
     res.send("Hello!");
-
 });
