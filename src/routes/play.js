1a7c20924f1625c33a2016fc54d701992bacdb04
Kana <68487042+kanagrou@users.noreply.github.com>
2022-12-06 01:48:20 +0000

    `HTML` + `CSS` base game

--- src/routes/play.js
+++ src/routes/play.js
@@ -3,7 +3,24 @@ const router = express.Router();
 
 
 router.get("/", (req, res) => {
-    res.send("Play!");
+    const game = {
+        teams: [
+            {name: "Team 1", score: 300},
+            {name: "Team 2", score: 250}
+        ],
+        question: {
+            name: "Question 1",
+            answers: [
+                {name: "Answer 1", value: 90},
+                {name: "Answer 2", value: 60},
+                {name: "Answer 3", value: 30},
+                {name: "Answer 4", value: 15},
+                {name: "Answer 5", value: 5}
+            ]
+        }
+    }
+
+    res.render("play", {game});
 });
 
 module.exports = router;
