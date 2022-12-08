6eec19d78477a2b6e10f0f72799f7b34ec80c7e4
Kana <68487042+kanagrou@users.noreply.github.com>
2022-12-07 21:59:38 -0500

    Port fix 3

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const fs = require("fs");
 
-const PORT = 8443;
+const PORT = 3000;
 const app = express();
 const server = app.listen(PORT, () => console.log("Listening on port", PORT));
 
